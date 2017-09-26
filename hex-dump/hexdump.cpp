376c9b5d0c648ef8f6b5b2c4de8395d41eb42311
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-09-25 19:35:15 -0500

    Start on program

--- hex-dump/hexdump.cpp
+++ hex-dump/hexdump.cpp
@@ -1,11 +1,45 @@
-// hexdump.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <fstream>
 
+using std::cout;
+using std::cin;
+using std::endl;
 
-int main()
-{
+using std::string;
+
+using std::ifstream;
+using std::ios;
+
+int main() {
+	string fileName;
+
+	do {
+		cout << "File name or e to exit: ";
+		cin >> fileName;
+
+		ifstream file;
+		file.open(fileName, ios::in | ios::binary);
+
+		if (!file.is_open()) {
+			cout << "Error opening file" << endl;
+			continue;
+		}
+
+		int currentLine = 0;
+		while (file.good() && !file.eof()) {
+			char* memory = new char[16];
+			file.read(memory, 16);
+			cout << memory;
+			file.seekg(16, ios::cur);
+		}
+
+		file.close();
+		cout << endl << endl;
+	} while (fileName != "e");
+
+	system("pause");
     return 0;
 }
 
